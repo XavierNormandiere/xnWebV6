2ef78e44dc84eb83ea977bbc4072e2a87c84cda1
XavierNormandiere <xnormandiere@gmail.com>
2022-03-23 15:21:20 +0100

    Contact controller et twigs

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/contact", name="contact_")
+ */
+class ContactController extends AbstractController
+{
+    /**
+     * @param ContactRepository $repository
+     * @return Response
+     * @Route("/", name="contacts")
+     */
+    public function contacts(ContactRepository $repository): Response
+    {
+        $contacts = $repository->findAll();
+        return $this->render('contact/contacts.html.twig', [
+            'contacts' => $contacts,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     * @Route("/formulaire", name="formulaire")
+     */
+    public function createContact(Request $request, EntityManagerInterface $em): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em->persist($contact);
+            $em->flush();
+            $this->addFlash('success', 'Enregistrement réussi');
+            return $this->redirectToRoute('main_home');
+        }
+        $formView = $form->createView();
+        return $this->render('contact/formulaire.html.twig', [
+            'form'=>$formView
+        ]);
+    }
+
+}
---
+++ src/Entity/Contact.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30, nullable=true)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
